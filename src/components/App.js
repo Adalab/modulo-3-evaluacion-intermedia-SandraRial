b682a4c04ba011f636229a038519860515ae1a6d
SandraRial <sandrarialperez@gmail.com>
2022-04-26 09:33:20 +0200

    add new phrase

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,26 @@ import { useState } from 'react';
 
 function App() {
   const [data, setData] = useState(phrasesList);
+  const [newPhrase, setNewPhrase] = useState({
+    quote: '',
+    character: '',
+  });
+
+  const handleNewPhrase = (ev) => {
+    setNewPhrase({
+      ...newPhrase,
+      [ev.target.id]: ev.target.value,
+    });
+  };
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setData([...data, newPhrase]);
+    setNewPhrase({
+      quote: '',
+      character: '',
+    });
+  };
 
   const htmlData = data.map((phrase, i) => {
     return (
@@ -27,6 +47,31 @@ function App() {
       <main>
         {/* list of phrases */}
         <ul>{htmlData}</ul>
+        {/* add new phrase */}
+        <form action="">
+          <h2>Añadir una nueva frase</h2>
+          <label htmlFor="quote">Frase</label>
+          <input
+            type="text"
+            name="quote"
+            id="quote"
+            value={newPhrase.quote}
+            onChange={handleNewPhrase}
+          />
+          <label htmlFor="character">Personaje</label>
+          <input
+            type="text"
+            name="character"
+            id="character"
+            value={newPhrase.character}
+            onChange={handleNewPhrase}
+          />
+          <input
+            type="submit"
+            value="Añadir una nueva frase"
+            onClick={handleClick}
+          />
+        </form>
       </main>
     </div>
   );
